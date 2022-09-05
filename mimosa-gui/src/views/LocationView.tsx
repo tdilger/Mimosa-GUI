7adfcbce72e58ca5f6b55d87a95537acc7eda344
tdilger <t.dilger@uni-bielefeld.de>
2022-09-05 09:12:28 +0200

    Adjusted keeping aspect ratio in location view

--- mimosa-gui/src/views/LocationView.tsx
+++ mimosa-gui/src/views/LocationView.tsx
@@ -65,9 +65,9 @@ export const LocationView: Component = () => {
      * Changed by LocationMenu by switching location via LocationCard
      */
     return (
-        <div id="locationView" class="z-0 relative container text-center m-auto overflow-hidden outline-dashed">
+        <div id="locationView" class="z-0 relative text-center m-auto overflow-hidden outline-dashed">
             <h2 class="text-3xl py-5">{ current_location().name }</h2>
-            <div id="locationDisplay" class="relative outline-dashed m-1" 
+            <div id="locationDisplay" class="relative m-auto outline-dashed" 
             style={{'width': `${ viewWidth() }em`, 'height': `${ viewHeight() }em`, 'aspect-ratio': `${ current_location().width } / ${ current_location().height }` }}>
                 <ItemDisplay viewport={ {width:viewWidth(), height:viewHeight()} } location={ current_location() } />
             </div>
