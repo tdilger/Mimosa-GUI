e56aeec521b6ff3d8dbeb380a50d8df302d8b70a
tdilger <t.dilger@uni-bielefeld.de>
2022-09-09 00:55:00 +0200

    Added speechinput icon

--- mimosa-gui/src/components/SpeechInput.tsx
+++ mimosa-gui/src/components/SpeechInput.tsx
@@ -1,10 +1,16 @@
+import IconButton from '@suid/material/IconButton';
+import MicIcon from "@suid/icons-material/Mic";
 import { Component } from 'solid-js';
 
 const SpeechInput: Component = () => {
     return (
-        <div id="speechInput" class="absolute bottom-10 mx-auto left-0 right-0 text-center outline-dotted">
-            <div class="speechInputContent">
-                <h2>SpeechInput</h2>
+        <div id="speechInput" class="absolute bottom-8 m-auto left-0 right-0 text-center align-middle outline-dotted">
+            <div class="bg-blue b w-full h-full rounded-3xl">
+                <div id="speechInputContent">
+                    <IconButton aria-label="speech-input" color="primary">
+                            <MicIcon sx={{ fontSize: 50 }} />
+                    </IconButton>
+                </div>
             </div>
         </div>
     )
